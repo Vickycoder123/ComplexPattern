ecec2a72bea90c3bff1b13e3b8a1fad3b6c63119
Vickycoder123 <vikaskumarg800@gmail.com>
2023-02-04 16:33:02 +0530

    DiamondINBorder pattern added

---
+++ DiamondInBorder.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class DiamondInBorder{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for( int i = 0; i < n; i++){
+            for( int j = 0; j < n; j++){
+                if( i+j == (n-1)/2 || i-j == (n-1)/2 || i+j == 3*(n-1)/2 || j-i == (n-1)/2 || i == 0 || j == 0 || i == n-1 || j == n-1){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
